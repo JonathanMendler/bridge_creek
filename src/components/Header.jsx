c64b640edf2458ef58551638904c37a91d292834
Jonathan Mendler <jon.mendler@gmail.com>
2024-05-22 10:53:08 -0500

    Remove LazyLoad from Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
